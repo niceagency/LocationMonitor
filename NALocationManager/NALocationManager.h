015e0fb071271c84ad8fc2997535220af025cfcc
Tim Searle <tim.searle@nice.agency>
2016-04-18 13:21:47 +0100

    Initial Commit

---
+++ NALocationManager/NALocationManager.h
@@ -0,0 +1,19 @@
+//
+//  NALocationManager.h
+//  NALocationManager
+//
+//  Created by Tim Searle on 18/04/2016.
+//  Copyright © 2016 Tim Searle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NALocationManager.
+FOUNDATION_EXPORT double NALocationManagerVersionNumber;
+
+//! Project version string for NALocationManager.
+FOUNDATION_EXPORT const unsigned char NALocationManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NALocationManager/PublicHeader.h>
+
+
