16eb9b38ce901e6586fa555fc3e0bae9f41b1b6c
Tim Searle <tim.searle@nice.agency>
2016-04-22 17:24:30 +0100

    Adding Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  LocationMonitor
+//
+//  Created by Tim Searle on 22/04/2016.
+//  Copyright © 2016 Tim Searle. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "LocationMonitor"
+)
